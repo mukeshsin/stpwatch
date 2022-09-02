bc1ab8af2eb1633f614b09522ec79d3791dfcf87
mukeshsin <mukesh@innotechteam.in>
2022-09-02 19:55:20 +0530

    lap button and hours add

--- stopwatch.js
+++ stopwatch.js
@@ -1,11 +1,14 @@
+let hour = 00;
 let minute = 00;
 let second = 00;
+let outputHours = document.querySelector(".hour");
 let outputMinutes = document.querySelector(".minute");
 let outputSeconds = document.querySelector(".second");
 let buttonStart = document.querySelector(".btn-first");
-console.log(buttonStart);
 let buttonStop = document.querySelector(".btn-middle");
 let buttonReset = document.querySelector(".btn-last");
+let lapBtn = document.querySelector(".btn-lap");
+let laps = document.querySelector(".laps");
 let Interval;
 
 buttonStart.addEventListener("click", () => {
@@ -13,34 +16,60 @@ buttonStart.addEventListener("click", () => {
     clearInterval(Interval)
     Interval = setInterval(startTime, 10);
 });
-buttonStop.addEventListener("click", () => {
-    clearInterval(Interval)
 
+buttonStop.addEventListener("click", () => {
+    clearInterval(Interval);
 });
+
 buttonReset.addEventListener("click", () => {
     clearInterval(Interval)
+    hour = "00";
     second = "00";
     minute = "00";
-    outputMinutes.innerHTML = minute;
-    outputSeconds.innerHTML = second;
+    outputHours.innerText = hour;
+    outputMinutes.innerText = minute;
+    outputSeconds.innerText = second;
+    laps.innerText = null;
 
 });
+
+lapBtn.addEventListener("click", () => {
+    const lap = document.createElement("div")
+    lap.innerText = hour + ":" + minute + ":" + second;
+    laps.appendChild(lap);
+});
+console.log(laps);
+
+
+
 
 function startTime() {
     second++;
     if (second <= 9) {
-        outputSeconds.innerHTML = "0" + second;
+        outputSeconds.innerText = "0" + second;
     }
     if (second > 9) {
-        outputSeconds.innerHTML = second;
+        outputSeconds.innerText = second;
     }
     if (second > 99) {
         minute++;
-        outputMinutes.innerHTML = "0" + minute;
-        second =0;
-        outputSeconds.innerHTML = "0" + second;
+        outputMinutes.innerText = "0" + minute;
+        second = 0;
+        outputSeconds.innerText = "0" + second;
     }
     if (minute > 9) {
-        outputMinutes.innerHTML = minute;
+        outputMinutes.innerText = minute;
+    }
+    if (minute > 59) {
+        hour++
+        outputHours.innerText = "0" + hour;
+        minute = 0;
+        outputMinutes.innerText = "0" + minute;
+        second = 0;
+        outputSeconds.innerText = "0" + second;
     }
 };
+// function lap(){
+//     lapNow=displayHour+":"+displayMinute+":"+displaySecond; 
+//     document.getElementById('lapRecord').innerHTML=document.getElementById('lapRecord')+"<p>"+lapNow+"</p>"; 
+laps.innerText = '';
