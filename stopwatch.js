bd4b45f6a7d0c1565742dfff33bf17ef53ff2d13
mukeshsin <mukesh@innotechteam.in>
2022-09-05 13:14:23 +0530

    all changes done

--- stopwatch.js
+++ stopwatch.js
@@ -1,9 +1,11 @@
 let hour = 00;
 let minute = 00;
 let second = 00;
+let milliSecond = 00;
 let outputHours = document.querySelector(".hour");
 let outputMinutes = document.querySelector(".minute");
 let outputSeconds = document.querySelector(".second");
+let outputmilliSeconds = document.querySelector(".milliSecond");
 let buttonStart = document.querySelector(".btn-first");
 let buttonStop = document.querySelector(".btn-middle");
 let buttonReset = document.querySelector(".btn-last");
@@ -11,10 +13,14 @@ let lapBtn = document.querySelector(".btn-lap");
 let laps = document.querySelector(".laps");
 let Interval;
 
+
+
 buttonStart.addEventListener("click", () => {
     console.log(buttonStart);
     clearInterval(Interval)
     Interval = setInterval(startTime, 10);
+    lapBtn.style.display="";
+
 });
 
 buttonStop.addEventListener("click", () => {
@@ -26,39 +32,43 @@ buttonReset.addEventListener("click", () => {
     hour = "00";
     second = "00";
     minute = "00";
+    milliSecond = "00";
     outputHours.innerText = hour;
     outputMinutes.innerText = minute;
     outputSeconds.innerText = second;
-    laps.innerText = null;
-
+    outputmilliSeconds.innerText = milliSecond;
+    laps.innerText = "";
+    lapBtn.style.display="none";
 });
 
 lapBtn.addEventListener("click", () => {
     const lap = document.createElement("div")
-    lap.innerText = hour + ":" + minute + ":" + second;
+    lap.innerText = hour + ":" + minute + ":" + second + ":" + milliSecond;
     laps.appendChild(lap);
+
 });
-console.log(laps);
-
-
-
 
 function startTime() {
-    second++;
-    if (second <= 9) {
-        outputSeconds.innerText = "0" + second;
+    milliSecond++;
+    if (milliSecond <= 9) {
+        outputmilliSeconds.innerText = "0" + milliSecond;
     }
-    if (second > 9) {
+    if (milliSecond > 9) {
+        outputmilliSeconds.innerText = milliSecond;
+    }
+    if (milliSecond > 99) {
+        second++;
+        outputSeconds.innerText = "0" + second;
+        milliSecond = 0;
+        outputmilliSeconds.innerText = "0" + milliSecond;
+    } if (second > 9) {
         outputSeconds.innerText = second;
     }
-    if (second > 99) {
-        minute++;
+    if (second > 59) {
+        minute++
         outputMinutes.innerText = "0" + minute;
         second = 0;
         outputSeconds.innerText = "0" + second;
-    }
-    if (minute > 9) {
-        outputMinutes.innerText = minute;
     }
     if (minute > 59) {
         hour++
@@ -67,9 +77,60 @@ function startTime() {
         outputMinutes.innerText = "0" + minute;
         second = 0;
         outputSeconds.innerText = "0" + second;
+        milliSecond = 0;
+        outputmilliSeconds.innerText = "0" + milliSecond;
     }
-};
-// function lap(){
-//     lapNow=displayHour+":"+displayMinute+":"+displaySecond; 
-//     document.getElementById('lapRecord').innerHTML=document.getElementById('lapRecord')+"<p>"+lapNow+"</p>"; 
-laps.innerText = '';
+}
+
+// function lapTime() {
+//     if (buttonStart = stopped);
+//     laps.innerText="";
+//         laps.innerText=hour + ":" + minute + ":" + second + ":" + milliSecond;
+//     };
+
+// function lapTime(){
+//     if(buttonStart===stopped);
+//     lapTime;
+//     document.getElementById("laps")=null;
+// }
+
+// function lapTime(){
+//     if(lapTime=hour="00",
+//     minute="00",
+//     second="00",
+//     milliSecond="00");
+//     document.getElementById("btn-lap").style.display=none;
+
+
+
+// }
+
+
+// function lapTime() {
+//     if (hour = 00,
+//         minute = "00",
+//         second = "00",
+//         milliSecond = "00");
+
+//     document.getElementById("btn-lap").style.display = "block";
+//     laps.innerText = '';
+
+// };
+// if(time === "stopped"){
+//     document.getElementById("btn-lap").style.display = "block";
+
+// function lapTime() {
+//     if (document.querySelector(".laps").innerText = "0") {
+//         lapBtn.disabled = true; //button remains disabled
+//     } else {
+//         lapBtn.disabled = false; //button is enabled
+//     }
+// }
+
+function start() {
+    document.getElementById("start").disabled = true;
+    document.getElementById("stop").disabled = false;
+    document.getElementById("lap").disabled = true;
+    document.getElementById("reset").disabled = false;
+    Interval = setInterval(startTime, 10);
+}
